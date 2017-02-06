a8b9c5f11da55adbb13dcf2dee325aa1ce26d346
Jacob <jacoboakley21@gmail.com>
2017-02-06 07:13:20 -0500

    updated layout

--- js/body.jsx
+++ js/body.jsx
@@ -4,10 +4,10 @@ class About extends React.Component {
   render() {
     return (
       <div className='about'>
-        <h2>About Me</h2>
-        <p>
-          I was born to do one thing but, I haven't been able to find a way to get paid sitting on the couch and binge watching Netflix. So, I decided to start studying Frontend Web Development. If you ask my family, they will tell you how technically savvy I am. I don't think I would be bragging to much if I said I am the go to guy when they need someone to hook up their printers. If the word of my family isn't enough keep reading and I might surprise you.
-        </p>
+          <h1>About Me</h1>
+          <p>
+            I was born to do one thing but, I haven't been able to find a way to get paid sitting on the couch and binge watching Netflix. So, I decided to start studying Frontend Web Development. If you ask my family, they will tell you how technically savvy I am. I don't think I would be bragging to much if I said I am the go to guy when they need someone to hook up their printers. If the word of my family isn't enough keep reading and I might surprise you.
+          </p>
       </div>
     )
   }
--- js/header.jsx
+++ js/header.jsx
@@ -5,17 +5,14 @@ class Header extends React.Component {
     return (
       <div className='header'>
         <div className='title'>
-          <img src="https://avatars1.githubusercontent.com/u/19611074?v=3&u=40349c6968cc661e50fa97e1f691c8a7668aecfc&s=140" />
-          <ul>
-            <li>Jacob Oakley</li>
-            <li>Front-end Developer </li>
-          </ul>
+            <p>Jacob Oakley</p>
+            <p>Front-end Developer </p>
         </div>
-        <ul className='link'>
-          <li><a href='#'>About Me</a></li>
-          <li><a href='#'>My Skills</a></li>
-          <li><a href='#'>My Work</a></li>
-        </ul>
+        <div className='link'>
+          <a href='#'>About Me</a>
+          <a href='#'>My Skills</a>
+          <a href='#'>My Work</a>
+        </div>
       </div>
     )
   }
