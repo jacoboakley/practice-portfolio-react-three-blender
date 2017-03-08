6fa6cf94ff2cb930c2d6b04abb13c766a5c5e2a3
Jacob <jacoboakley21@gmail.com>
2017-03-08 07:27:26 -0500

    added address to links on footer

--- jsx/footer.jsx
+++ jsx/footer.jsx
@@ -35,18 +35,18 @@ class Footer extends React.Component {
         
         <div id='footer'>
           <div id='code'>
-            <img src='assets/icons/github.png' />
-            <img src='assets/icons/codepen.png' />
-            <img src='assets/icons/document.png' />
+            <a href='https://medium.com/@jacoboakley21' target='_blank'><img src='assets/icons/medium.png' /></a>
+            <a href='https://twitter.com/jacoboakley21' target='_blank'><img src='assets/icons/twitter.png' /></a>
+            <a href='http://codepen.io/jacoboakley/#' target='_blank'><img src='assets/icons/codepen.png' /></a>
           </div>
           <div id='selector'>
           <a href='#'data-value='0' onClick={this.select.bind(this)} />
           <a href='#'data-value='1' onClick={this.select.bind(this)} />
           </div>
           <div id='social'>
-            <img src='assets/icons/twitter.png' />
-            <img src='assets/icons/medium.png' />
-            <img src='assets/icons/linkedin.png' />
+            <a href='assets/other/resume.docx'><img src='assets/icons/document.png' /></a>
+            <a href='https://github.com/jacoboakley' target='_blank'><img src='assets/icons/github.png' /></a>
+            <a href='https://www.linkedin.com/in/jacoboakley' target='_blank'><img src='assets/icons/linkedin.png' /></a>
           </div>
         </div>
         
